998abffed809ac07a749f0720ada0f516c7ac310
Qiao <SugarQiao@outlook.com>
2026-04-19 16:02:03 +0800

    refactor(examples): 补处理遗漏的 line_edit demo
    替换 JoJo 数据为编程语言列表，添加 docstring 与信号交互。

--- examples/widgets/line_edit/demo.py
+++ examples/widgets/line_edit/demo.py
@@ -1,56 +1,73 @@
 # coding:utf-8
+"""
+LineEdit 演示
+
+展示内容：
+- LineEdit 单行输入框
+- SearchLineEdit 搜索输入框
+- QCompleter 自动补全
+- 清除按钮与占位符文本
+- 主题切换
+"""
 import sys
 
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QCompleter
-from qfluentwidgets import LineEdit, PushButton, SearchLineEdit, setTheme, Theme
+
+from qfluentwidgets import LineEdit, PushButton, SearchLineEdit, BodyLabel, setTheme, Theme
 
 
 class Demo(QWidget):
+    """LineEdit 演示窗口"""
 
     def __init__(self):
         super().__init__()
-        # self.setStyleSheet("Demo {background: rgb(32, 32, 32)}")
-        # setTheme(Theme.DARK)
+        self.setWindowTitle('LineEdit - 演示')
+        self.resize(400, 300)
+        self.initWidgets()
+        self.initLayout()
 
-        self.hBoxLayout = QHBoxLayout(self)
+    def initWidgets(self):
+        """初始化输入框"""
+        self.statusLabel = BodyLabel('请输入内容进行搜索', self)
+        self.statusLabel.setAlignment(Qt.AlignCenter)
+
         self.lineEdit = SearchLineEdit(self)
-        self.button = PushButton('Search', self)
+        self.button = PushButton('搜索', self)
 
-        # add completer
-        stands = [
-            "Star Platinum", "Hierophant Green",
-            "Made in Haven", "King Crimson",
-            "Silver Chariot", "Crazy diamond",
-            "Metallica", "Another One Bites The Dust",
-            "Heaven's Door", "Killer Queen",
-            "The Grateful Dead", "Stone Free",
-            "The World", "Sticky Fingers",
-            "Ozone Baby", "Love Love Deluxe",
-            "Hermit Purple", "Gold Experience",
-            "King Nothing", "Paper Moon King",
-            "Scary Monster", "Mandom",
-            "20th Century Boy", "Tusk Act 4",
-            "Ball Breaker", "Sex Pistols",
-            "D4C • Love Train", "Born This Way",
-            "SOFT & WET", "Paisley Park",
-            "Wonder of U", "Walking Heart",
-            "Cream Starter", "November Rain",
-            "Smooth Operators", "The Matte Kudasai"
+        # 自动补全数据
+        searchTerms = [
+            'Python', 'JavaScript', 'TypeScript', 'Go', 'Rust',
+            'C++', 'Java', 'C#', 'Swift', 'Kotlin',
+            'Ruby', 'PHP', 'Lua', 'Dart', 'Scala',
         ]
-        self.completer = QCompleter(stands, self.lineEdit)
+        self.completer = QCompleter(searchTerms, self.lineEdit)
         self.completer.setCaseSensitivity(Qt.CaseInsensitive)
         self.completer.setMaxVisibleItems(10)
         self.lineEdit.setCompleter(self.completer)
 
-        self.resize(400, 400)
-        self.hBoxLayout.setAlignment(Qt.AlignCenter)
-        self.hBoxLayout.addWidget(self.lineEdit, 0, Qt.AlignCenter)
-        self.hBoxLayout.addWidget(self.button, 0, Qt.AlignCenter)
-
         self.lineEdit.setFixedSize(200, 33)
         self.lineEdit.setClearButtonEnabled(True)
-        self.lineEdit.setPlaceholderText('Search stand')
+        self.lineEdit.setPlaceholderText('搜索编程语言')
+        self.lineEdit.returnPressed.connect(self.onSearch)
+        self.button.clicked.connect(self.onSearch)
+
+    def initLayout(self):
+        """初始化布局"""
+        mainLayout = QHBoxLayout(self)
+        mainLayout.setSpacing(12)
+        mainLayout.setContentsMargins(30, 30, 30, 30)
+        mainLayout.addWidget(self.statusLabel)
+        mainLayout.addWidget(self.lineEdit, 0, Qt.AlignCenter)
+        mainLayout.addWidget(self.button, 0, Qt.AlignCenter)
+
+    def onSearch(self):
+        """搜索按钮点击"""
+        text = self.lineEdit.text()
+        if text:
+            self.statusLabel.setText(f'搜索: {text}')
+        else:
+            self.statusLabel.setText('请输入内容进行搜索')
 
 
 if __name__ == '__main__':
