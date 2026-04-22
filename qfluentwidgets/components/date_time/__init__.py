81fb63d31d3f0b62919832475101b5c27012d5eb
Qiao <SugarQiao@outlook.com>
2026-04-22 17:24:21 +0800

    chore(date-time): 导出 RangeCalendarPicker 到 date_time 模块

--- qfluentwidgets/components/date_time/__init__.py
+++ qfluentwidgets/components/date_time/__init__.py
@@ -4,3 +4,4 @@ from .calendar_picker import CalendarPicker, FastCalendarPicker
 from .date_picker import DatePickerBase, DatePicker, ZhDatePicker
 from .picker_base import PickerBase, PickerPanel, PickerColumnFormatter
 from .time_picker import TimePicker, AMTimePicker
+from .range_calendar_picker import RangeCalendarPicker, RangeCalendarView, RangeFastDayScrollItemDelegate
